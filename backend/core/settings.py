9b772796872f19f44af022259ff1a2a902086c4b
AbdeljalilOuafi <abdeljalilouafi44@gmail.com>
2025-01-10 00:22:18 +0100

    Update settings ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -33,7 +33,14 @@ SECRET_KEY = 'django-insecure-+g$9@jca@f#&6+&$@00td(dxi!2mwo%_^!4*soe+s3+4sc&6)t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'prime-eats-backend.onrender.com',
+    'primeeats.live',
+    'www.primeeats.live',
+    'api.primeeats.live',
+]
 
 # Application definition
 
