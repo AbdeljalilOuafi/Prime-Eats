4ed262f5ec989ebe118d6e9d6e2f8e4d6ec0ada4
AbdeljalilOuafi <89494135+AbdeljalilOuafi@users.noreply.github.com>
2025-01-10 11:37:11 +0100

    Add corsheader Middleware to deployement_settings.py to allow domain make requests to the API

--- backend/core/deployement_settings.py
+++ backend/core/deployement_settings.py
@@ -40,6 +40,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') #MAKE SECRET_KEY AS ENVIRONMENT VARIAB
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
